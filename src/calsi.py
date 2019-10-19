2f71ffb977c4446e1b0e8fe6579a3adf14fa5d50
Vinay Rana <38176185+VinayRana5674@users.noreply.github.com>
2019-10-19 23:02:50 +0530

    Update calsi.py

--- src/calsi.py
+++ src/calsi.py
@@ -1,26 +1,12 @@
-def add(x, y):
-   return x + y
-def subtract(x, y):
-   return x - y
-def multiply(x, y):
-   return x * y
-def divide(x, y):
-   return x / y
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-choice = input("Enter choice(1/2/3/4):")
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
+num = 7
+
+factorial = 1
+
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
 else:
-   print("Invalid input")
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
